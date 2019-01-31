a9693ca1a74c03a5c0d6b045f883542700f50878
Daniel <danielstarling4@gmail.com>
2019-01-30 21:38:40 -0700

    playing with JSON data

---
+++ vocab.js
@@ -0,0 +1,42 @@
+
+vocab = [
+    {
+        "name": "Template Literals",
+        "definition": "Template literals are string literals allowing embedded expressions.",
+        "example": "`string text ${expression} string text`",
+        "description": "Template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. Template literals can contain placeholders. These are indicated by the dollar sign and curly braces (${expression}).",
+        "relevant-links": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+    },
+    {
+        "name": "Template Literals",
+        "definition": "Template literals are string literals allowing embedded expressions.",
+        "example": "`string text ${expression} string text`",
+        "description": "Template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. Template literals can contain placeholders. These are indicated by the dollar sign and curly braces (${expression}).",
+        "relevant-links": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+    },
+    {
+        "name": "Template Literals",
+        "definition": "Template literals are string literals allowing embedded expressions.",
+        "example": "`string text ${expression} string text`",
+        "description": "Template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. Template literals can contain placeholders. These are indicated by the dollar sign and curly braces (${expression}).",
+        "relevant-links": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+    },
+    {
+        "name": "Template Literals",
+        "definition": "Template literals are string literals allowing embedded expressions.",
+        "example": "`string text ${expression} string text`",
+        "description": "Template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. Template literals can contain placeholders. These are indicated by the dollar sign and curly braces (${expression}).",
+        "relevant-links": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+    },
+    {
+        "name": "Template Literals",
+        "definition": "Template literals are string literals allowing embedded expressions.",
+        "example": "`string text ${expression} string text`",
+        "description": "Template literals are enclosed by the back-tick (` `)  (grave accent) character instead of double or single quotes. Template literals can contain placeholders. These are indicated by the dollar sign and curly braces (${expression}).",
+        "relevant-links": "https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals"
+    }
+]
+
+
+
+console.log(vocab.toString())
