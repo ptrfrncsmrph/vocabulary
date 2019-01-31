ef8fdf2c530aa010a164962d33f748e710a1db30
Peter Murphy <peter.francis.murphy@gmail.com>
2019-01-31 09:29:37 -0500

    Updates entry.spec.json to test all of the entries in vocab.json

--- entry.spec.js
+++ entry.spec.js
@@ -12,11 +12,11 @@ const requiredFields = [
 const validateEntry = entry => {
   // Make sure that the entry contains at least all the required fields
   requiredFields.forEach(field => {
-    assert(entry[field], `Missing required field: ${field}`)
+    assert(entry[field], `Test failed for ${entry.name || "unnamed entry"}\nMissing required field: ${field}`)
   })
   // Check that there are no additional fields (not sure if you want to do that)
   Object.keys(entry).forEach(key => {
-    assert(requiredFields.includes(key), `Additional field found: ${key}`)
+    assert(requiredFields.includes(key), `Test failed for ${entry.name || "unnamed entry"}\nAdditional field found: ${key}`)
   })
   console.log(`All tests passing!`)
 }
